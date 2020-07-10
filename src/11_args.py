1eecb42f84cf833da89c0ca64e1b984dc2cbe615
Pedro Revilla <peterevilla@gmail.com>
2020-07-09 21:59:40 -0500

    wip

--- src/11_args.py
+++ src/11_args.py
@@ -24,7 +24,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(tuple(a)))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -32,6 +32,8 @@ print(f2(tuple(a)))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2 = 1):
+    return num1 + num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -46,7 +48,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(a, b):
+    print(f'key: a, value: {a}')
+    print(f'key: b, value: {b}')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
